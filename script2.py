9ce822c6f8c781eb2eaf876116112fc67ea13f24
Jaskaran Singh <snak3codes@outlook.com>
2019-08-10 13:15:26 -0400

    Removed some code

--- script2.py
+++ script2.py
@@ -1,20 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-# print(sys.version)
-# print(sys.executable)
-
-
-def greet(who):
-    test = "test"
-    greeting = "Hello,{}".format(who)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Corey'))
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
